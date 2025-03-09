45d0fcd9a51807bdc596bc5f85f309d12a3b0065
Adam Heeps <adam.heeps@live.co.uk>
2025-03-09 23:13:32 +0000

    feat: repo init - steam auth and logout

--- src/pages/dashboard.tsx
+++
@@ -1,8 +0,0 @@
-export default function Dashboard() {
-    return (
-        <div style={{ textAlign: 'center', padding: '2rem' }}>
-            <h1>Dashboard</h1>
-            <p>You are successfully signed in via Steam!</p>
-        </div>
-    );
-}
